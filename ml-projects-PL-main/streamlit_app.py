0132fec2be21282c6ee58ab3109150a4d90248f0
PorscheLiao <shih.liao@hotmail.com>
2024-08-20 18:54:48 +0800

    V1.2

--- ml-projects-PL-main/streamlit_app.py
+++ ml-projects-PL-main/streamlit_app.py
@@ -11,8 +11,9 @@ def get_image_html(page_name, file_name):
     data_url = base64.b64encode(contents).decode("utf-8")
     return f'<a href="{page_name}"><img src="data:image/png;base64,{data_url}" style="width:300px"></a>'
 
-data_url_2 = get_image_html("分類", "ml-projects-PL-main/iris.png")
-data_url_3 = get_image_html("迴歸", "ml-projects-PL-main/taxi.png")
+data_url_2 = get_image_html("分類", "./iris.png")
+data_url_3 = get_image_html("迴歸", "./taxi.png")
+data_url_4 = get_image_html("辨識", "./mnist.png")
 
 st.set_page_config(
     page_title="我的學習歷程",
@@ -21,20 +22,8 @@ st.set_page_config(
 
 st.title('Machine Learning 學習歷程')   
 
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2 = st.columns(2)
 with col1:
-    
-    st.markdown('### [(聯立方程式解)聯立方程式解](聯立方程式解)')
-    st.markdown('''
-    ##### 方程式1
-          方程式2
-    ##### 求解
-          x=?
-          y=?
-        ''')
-    
-
-with col2:
     # url must be external url instead of local file
     # st.markdown(f"### [![分類]({url})](分類)")
     st.markdown('### [(分類)企鵝品種辨識](分類)')
@@ -53,30 +42,8 @@ with col2:
         ''')
     # st.image('iris.png')
     st.markdown(data_url_2, unsafe_allow_html=True)
-with col3:
-    st.markdown('### [(迴歸)計程車小費預測](迴歸)')
-    st.markdown('''
-    ##### 特徵(X):
-        - 車費
-        - 性別
-        - 吸菸
-        - 星期
-        - 時間
-        - 同行人數
-    ##### 目標：預測小費金額
-        ''')
-    # st.image('taxi.png')
-    st.markdown(data_url_3, unsafe_allow_html=True)
-with col4:
-    # url must be external url instead of local file
-    # st.markdown(f"### [![辨識]({url})](辨識)")
-    st.markdown('### [(辨識)辨識手寫字母與阿拉伯數字](辨識)')
-    st.markdown('''
-    ##### 辨識手寫字母與阿拉伯數字
-        ''')
-   
-with col5: 
-        # st.markdown(f"### [![分類]({url})](分類)")
+
+
     st.markdown('### [(分類)BreastCancer_prediction](分類)')
     st.markdown('''
     ##### 特徵(X):
@@ -114,5 +81,38 @@ with col5:
         - malignant
         - benign
         ''')
- 
+
+    
+    st.markdown('### [(聯立方程式解)聯立方程式解](聯立方程式解)')
+    st.markdown('''
+    ##### 方程式1
+          方程式2
+    ##### 求解
+          x=?
+          y=?
+        ''')
+    
+with col2:
+    st.markdown('### [(迴歸)計程車小費預測](迴歸)')
+    st.markdown('''
+    ##### 特徵(X):
+        - 車費
+        - 性別
+        - 吸菸
+        - 星期
+        - 時間
+        - 同行人數
+    ##### 目標：預測小費金額
+        ''')
+    # st.image('taxi.png')
+    st.markdown(data_url_3, unsafe_allow_html=True)
+
+
+    # url must be external url instead of local file
+    # st.markdown(f"### [![辨識]({url})](辨識)")
+    st.markdown('### [(辨識)辨識手寫字母與阿拉伯數字](辨識)')
+    st.markdown('''
+    ##### 辨識手寫字母與阿拉伯數字
+        ''')
+    st.markdown(data_url_4, unsafe_allow_html=True)
  
